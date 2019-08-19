7c46f43c2fdc99e04c1075d00d46eaa4bbd76c78
Kei Nohguchi <kei@nohguchi.com>
2019-08-19 13:59:49 -0700

    factorial: Use the global variables for the command line arguments
    Just to make the code as simple as possible.

--- factorial.asm
+++ factorial.asm
@@ -1,55 +1,45 @@
 ; SPDX-License-Identifier: GPL-2.0
 	segment	.data
+prognam	dq	0
+n	dq	4
+want	dq	24
 fmt	db	"%s: %ld! == %ld", 0xa, 0
 failfmt	db	"%s: %ld!(%ld) != %ld", 0xa, 0
-n	dq	4
-want	dq	24		; 4! == 24
 	segment	.text
 	global	main, factorial
 	extern	atoi, printf
 main	push	rbp
 	mov	rbp, rsp
-.arg0	equ	0
-.argv	equ	8
-.n	equ	16
-.want	equ	24
-	sub	rsp, 32
 	mov	rax, [rsi]
-	mov	[rsp+.arg0], rax
-	mov	[rsp+.argv], rsi
+	mov	[prognam], rax
 	cmp	rdi, 3
-	jne	.default
-	mov	rcx, [rsp+.argv]
-	mov	rdi, [rcx+8]
+	jne	.start
+	sub	rsp, 16
+	mov	[rsp], rsi
+	mov	rdi, [rsi+8]
 	call	atoi
-	mov	[rsp+.n], rax
-	mov	rcx, [rsp+.argv]
-	mov	rdi, [rcx+16]
+	mov	[n], rax
+	mov	rsi, [rsp]
+	mov	rdi, [rsi+16]
 	call	atoi
-	mov	[rsp+.want], rax
-	jmp	.start
-.default
-	mov	rax, [n]
-	mov	[rsp+.n], rax
-	mov	rax, [want]
-	mov	[rsp+.want], rax
-.start	mov	rdi, [rsp+.n]
+	mov	[want], rax
+.start	mov	rdi, [n]
 	mov	esi, 1
 	call	factorial
-	cmp	rax, [rsp+.want]
+	cmp	rax, [want]
 	jz	.pass
 	mov	rdi, failfmt
-	mov	rsi, [rsp+.arg0]
-	mov	rdx, [rsp+.n]
+	mov	rsi, [prognam]
+	mov	rdx, [n]
 	mov	rcx, rax
-	mov	r8, [rsp+.want]
+	mov	r8, [want]
 	xor	eax, eax
 	call	printf
 	mov	eax, 1
 	jmp	.out
 .pass	mov	rdi, fmt
-	mov	rsi, [rsp+.arg0]
-	mov	rdx, [rsp+.n]
+	mov	rsi, [prognam]
+	mov	rdx, [n]
 	mov	rcx, rax
 	xor	eax, eax
 	call	printf
