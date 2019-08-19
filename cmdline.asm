c26ead8ad7ece1146e3ffba30f3bea4a2764fd9b
Kei Nohguchi <kei@nohguchi.com>
2019-08-19 16:14:40 -0700

    cmdline: Cleanup the code to fit it less than 24 rows

--- cmdline.asm
+++ cmdline.asm
@@ -1,16 +1,16 @@
 ; SPDX-License-Identifier: GPL-2.0
 	section	.data
-fmt	db	"%s ", 0
+fmt	db	"%s", 0xa, 0
 	section	.text
 	global	main
-	extern	printf, putchar
+	extern	printf
 main	push	rbp
 	mov	rbp, rsp
 	sub	rsp, 16
 	mov	rcx, rsi
 	mov	rsi, [rcx]
-.more:	mov	rdi, fmt
-	mov	[rsp], rcx
+.more	mov	[rsp], rcx
+	mov	rdi, fmt
 	xor	eax, eax
 	call	printf
 	mov	rcx, [rsp]
@@ -18,8 +18,6 @@ main	push	rbp
 	mov	rsi, [rcx]
 	cmp	rsi, 0
 	jnz	.more
-	mov	edi, 0xa
-	call	putchar
 	xor	eax, eax
 	leave
 	ret
