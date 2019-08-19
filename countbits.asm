67f3ca17eff3539d60778b07d4f20d75dc021e1e
Kei Nohguchi <kei@nohguchi.com>
2019-08-19 10:48:57 -0700

    countbits: Use stack to keep the expected result

--- countbits.asm
+++ countbits.asm
@@ -1,27 +1,30 @@
 ; SPDX-License-Identifier: GPL-2.0
 	section	.data
-data	dq	0xfedcba9876543210
-sum	dq	0
+data	dq	0xfedcba9876543210	; example data for bit counting
+want	dd	32			; 32 ones in data above
 	section	.text
 	global	main
-main:
+	extern	atoi
+main
 	push	rbp
 	mov	rbp, rsp
-	xor	eax, eax
+	sub	rsp, 16
+	cmp	rdi, 3
+	jz	.done
+	mov	rdx, [data]
+	mov	eax, 32
+	mov	[rsp], rax
+.done	xor	eax, eax
 	xor	ebx, ebx
 	mov	ecx, 64
-	mov	rdx, [data]
-.while:
-	bt	edx, 0
+.while	bt	edx, 0
 	setc	bl
 	add	eax, ebx
 	shr	rdx, 1
 	sub	ecx, 1
 	jnz	.while
-	mov	[sum], rax
-	cmp	qword [sum], 32	; data should contains 32 ones
-	jnz	.err
+	cmp	eax, [rsp]
+	jne	.out
 	xor	eax, eax
-.err
-	leave
+.out	leave
 	ret
